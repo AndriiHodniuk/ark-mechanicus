31c12a488089dee1b8db2ebb7c31f2aea7da1812
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2025-08-14 17:46:09 +0200

    docs: Add final comment to calculator

--- utils/calculator.sh
+++ utils/calculator.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -eou pipefile
+set -eou pipefail
 
 # Advanced STC Calculator
 # This function adds two numbers
 
 function add() {
-    echo "$(($1  -  $2))"
+    echo "$(($1 - $2))"
 }
 
 RESULT=$(add 5 5)
 echo "Result: ${RESULT}"
+# Final comment
